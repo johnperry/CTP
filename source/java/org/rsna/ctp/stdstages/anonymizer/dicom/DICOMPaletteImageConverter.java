a828f512deb1bb1eaac70e8437383b1d80782ff6
John Perry <johnperry@dls.net>
2015-12-15 08:50:38 -0600

    Performance improvement in DICDOMPaletteImageConverter; move ServerStatusServlet to Util

--- source/java/org/rsna/ctp/stdstages/anonymizer/dicom/DICOMPaletteImageConverter.java
+++ source/java/org/rsna/ctp/stdstages/anonymizer/dicom/DICOMPaletteImageConverter.java
@@ -7,20 +7,23 @@
 
 package org.rsna.ctp.stdstages.anonymizer.dicom;
 
+import java.awt.image.*;
+import java.awt.geom.*;
+import java.awt.Graphics2D;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.EOFException;
+import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.security.*;
-import java.util.Arrays;
-import java.util.Calendar;
+import javax.imageio.*;
+import javax.imageio.stream.*;
 
 import org.dcm4che.data.Dataset;
 import org.dcm4che.data.DcmDecodeParam;
@@ -68,7 +71,7 @@ public class DICOMPaletteImageConverter {
 		logger.debug("File length       = "+fileLength);
 
 		BufferedInputStream in = null;
-		FileOutputStream out = null;
+		BufferedOutputStream out = null;
 		File tempFile = null;
 		byte[] buffer = new byte[4096];
 		try {
@@ -95,60 +98,72 @@ public class DICOMPaletteImageConverter {
 			int numberOfFrames = getInt(dataset, Tags.NumberOfFrames, 1);
 			int rows = getInt(dataset, Tags.Rows, 0);
 			int columns = getInt(dataset, Tags.Columns, 0);
-			int bitsAllocated = getInt(dataset, Tags.BitsAllocated, 16);
-			int samplesPerPixel = getInt(dataset, Tags.SamplesPerPixel, 1);
-			int planarConfiguration = getInt(dataset, Tags.PlanarConfiguration, 0);
 			String photometricInterpretation = getString(dataset, Tags.PhotometricInterpretation, "");
 			if ((rows == 0) || (columns == 0)) {
 				close(in);
 				return AnonymizerStatus.SKIP(inFile, "Unable to get the rows and columns");
 			}
-			if (samplesPerPixel != 1) {
-				close(in);
-				return AnonymizerStatus.SKIP(inFile, "Unsupported SamplesPerPixel: "+samplesPerPixel);
-			}
-			if (bitsAllocated != 8) {
-				close(in);
-				return AnonymizerStatus.SKIP(inFile, "Unsupported BitsAllocated: "+bitsAllocated);
-			}
-			if (planarConfiguration != 1) {
-				close(in);
-				return AnonymizerStatus.SKIP(inFile, "Unsupported PlanarConfiguration: "+planarConfiguration);
-			}
-			if (!photometricInterpretation.equals("RGB")) {
+			if (!photometricInterpretation.equals("PALETTE COLOR")) {
 				close(in);
 				return AnonymizerStatus.SKIP(inFile, "Unsupported PhotometricInterpretation: "+photometricInterpretation);
 			}
-
             if (parser.getReadTag() != Tags.PixelData) {
 				close(in);
 				return AnonymizerStatus.SKIP(inFile, "No pixels");
 			}
 
-			//Set the encoding
+
+			//Get the encoding and set the parameters
 			DcmDecodeParam fileParam = parser.getDcmDecodeParam();
-        	String prefEncodingUID = UIDs.ImplicitVRLittleEndian;
+        	String fileEncodingUID = UIDs.ImplicitVRLittleEndian;
 			FileMetaInfo fmi = dataset.getFileMetaInfo();
-            if (fmi != null) prefEncodingUID = fmi.getTransferSyntaxUID();
-			DcmEncodeParam encoding = (DcmEncodeParam)DcmDecodeParam.valueOf(prefEncodingUID);
-			boolean swap = fileParam.byteOrder != encoding.byteOrder;
+            if (fmi != null) fileEncodingUID = fmi.getTransferSyntaxUID();
+            boolean isBigEndian = fileEncodingUID.equals(UIDs.ExplicitVRBigEndian);
+            String encodingUID = UIDs.ExplicitVRLittleEndian;
+			DcmEncodeParam encoding = (DcmEncodeParam)DcmDecodeParam.valueOf(encodingUID);
+			boolean swap = (fileParam.byteOrder != encoding.byteOrder);
 
 			if (encoding.encapsulated) {
 				logger.debug("Encapsulated pixel data found");
 				close(in);
 				return AnonymizerStatus.SKIP(inFile, "Encapsulated pixel data not supported");
 			}
+			
+			//Get the LUTs
+			LUT red = new LUT(dataset.getInts(Tags.RedPaletteColorLUTDescriptor),
+							  dataset.getInts(Tags.RedPaletteColorLUTData));
+			LUT green = new LUT(dataset.getInts(Tags.GreenPaletteColorLUTDescriptor),
+							  dataset.getInts(Tags.GreenPaletteColorLUTData));
+			LUT blue = new LUT(dataset.getInts(Tags.BluePaletteColorLUTDescriptor),
+							  dataset.getInts(Tags.BluePaletteColorLUTData));
 
-			//Set PlanarConfiguration to 0
+			//Set the PlanarConfiguration to 0
 			dataset.putUS(Tags.PlanarConfiguration, 0);
+			
+			//Set the PhotometricInterpretation to RGB
+			dataset.putCS(Tags.PhotometricInterpretation, "RGB");
+			
+			//Set the pixel parameters
+			dataset.putUS(Tags.SamplesPerPixel, 3);
+			dataset.putUS(Tags.BitsAllocated, 8);
+			dataset.putUS(Tags.BitsStored, 8);
+			dataset.putUS(Tags.HighBit, 7);			
+			
+			//Remove the lookup tables and their descriptors
+			dataset.remove(Tags.RedPaletteColorLUTDescriptor);
+			dataset.remove(Tags.GreenPaletteColorLUTDescriptor);
+			dataset.remove(Tags.BluePaletteColorLUTDescriptor);
+			dataset.remove(Tags.RedPaletteColorLUTData);
+			dataset.remove(Tags.GreenPaletteColorLUTData);
+			dataset.remove(Tags.BluePaletteColorLUTData);
 
 			//Save the dataset to a temporary file, and rename at the end.
 			File tempDir = outFile.getParentFile();
 			tempFile = File.createTempFile("DCMtemp-", ".anon", tempDir);
-            out = new FileOutputStream(tempFile);
+            out = new BufferedOutputStream(new FileOutputStream(tempFile));
 
             //Create and write the metainfo for the encoding we are using
-			fmi = oFact.newFileMetaInfo(dataset, prefEncodingUID);
+			fmi = oFact.newFileMetaInfo(dataset, encodingUID);
             dataset.setFileMetaInfo(fmi);
             fmi.write(out);
 
@@ -156,39 +171,40 @@ public class DICOMPaletteImageConverter {
 			dataset.writeDataset(out, encoding);
 
 			//Process the pixels
+			int nPixels = numberOfFrames * rows * columns;
+			int nPixelBytes = nPixels * 3 /*samplesPerPixel*/;
+			int pad = nPixelBytes & 1;
 			dataset.writeHeader(
 				out,
 				encoding,
 				parser.getReadTag(),
-				parser.getReadVR(),
-				parser.getReadLength());
-			processPixels(parser, out, numberOfFrames, rows, columns);
+				VRs.OB,
+				nPixelBytes + pad);
+
+			int pd;
+			int b1,b2;
+			int bytesPerFrame = rows * columns * 2;
+			byte[] frameBytes = new byte[bytesPerFrame];
+			for (int frame=0; frame<numberOfFrames; frame++) {
+				if (in.read(frameBytes, 0, frameBytes.length) != bytesPerFrame) throw new Exception("End of File");
+				for (int p=0; p<bytesPerFrame; ) {
+					b1 = frameBytes[p++];
+					b2 = frameBytes[p++];
+					if (!swap) {
+						pd = ((b2 & 0xff)<<8) | (b1 & 0xff);
+					}
+					else {
+						pd = ((b1 & 0xff)<<8) | (b2 & 0xff);
+					}
+					out.write(red.get(pd));
+					out.write(green.get(pd));
+					out.write(blue.get(pd));
+				}
+			}
+			if (pad != 0) out.write(0);			
 			logger.debug("Finished writing the pixels");
 
-			 //Get ready for the next element
-			logger.debug("Stream position after processPixels = "+parser.getStreamPosition());
-			if (parser.getStreamPosition() < fileLength) parser.parseHeader();
-
-			//Now do any elements after the pixels one at a time.
-			//This is done to allow streaming of large raw data elements
-			//that occur above Tags.PixelData.
-			int tag;
-			while (!parser.hasSeenEOF()
-					&& (parser.getStreamPosition() < fileLength)
-						&& ((tag=parser.getReadTag()) != -1)
-							&& (tag != 0xFFFAFFFA)
-							&& (tag != 0xFFFCFFFC)) {
-				logger.debug("About to write post-pixels element "+Integer.toHexString(tag));
-				dataset.writeHeader(
-					out,
-					encoding,
-					parser.getReadTag(),
-					parser.getReadVR(),
-					parser.getReadLength());
-				writeValueTo(parser, buffer, out, swap);
-				parser.parseHeader();
-			}
-			logger.debug("Finished writing the post-pixels elements");
+			 //Skip everything after the pixels
 			out.flush();
 			out.close();
 			in.close();
@@ -198,7 +214,7 @@ public class DICOMPaletteImageConverter {
 		}
 
 		catch (Exception e) {
-			logger.debug("Exception while processing image.",e);
+			logger.debug("Exception while processing image.", e);
 
 			//Close the input stream if it actually got opened.
 			close(in);
@@ -236,99 +252,37 @@ public class DICOMPaletteImageConverter {
 		try { return ds.getString(tag); }
 		catch (Exception ex) { return defaultValue; }
 	}
-
-    private static void processPixels(
-							DcmParser parser,
-							OutputStream out,
-							int numberOfFrames,
-							int rows,
-							int columns) throws Exception {
-
-		int len = parser.getReadLength();
-		logger.debug("Read length       = "+len);
-
-		int size = rows * columns;
-		byte[] r = new byte[size];
-		byte[] g = new byte[size];
-		byte[] b = new byte[size];
-		byte[] rgb = new byte[3*size];
-
-		InputStream in = parser.getInputStream();
-		for (int frame=0; frame<numberOfFrames; frame++) {
-			getColor(in, r);
-			getColor(in, g);
-			getColor(in, b);
-			int ptr = 0;
-			for (int k=0; k<size; k++) {
-				rgb[ptr++] = r[k];
-				rgb[ptr++] = g[k];
-				rgb[ptr++] = b[k];
+	
+	static class LUT {
+		byte[] color;
+		int nEntries;
+		int firstValue;
+		int nBits;
+		
+		public LUT(int[] desc, int[] data) {
+			nEntries = desc[0];
+			if (nEntries == 0) nEntries = 65536;
+			firstValue = desc[1];
+			nBits = desc[2];
+			color = new byte[nEntries];
+			if (nBits == 8) {
+				for (int i=0; i<nEntries; i++) color[i] = (byte)data[i];
 			}
-			out.write(rgb, 0, rgb.length);
-		}
-		//Add a byte to the end if we have written an odd number of bytes
-		long nbytes = numberOfFrames * rows * columns * 3;
-		logger.debug("numberOfFrames    = "+numberOfFrames);
-		logger.debug("rows              = "+rows);
-		logger.debug("columns           = "+columns);
-		logger.debug("Total image bytes = "+nbytes);
-		if ((nbytes & 1) != 0) out.write(0);
-
-		parser.setStreamPosition(parser.getStreamPosition() + len);
-	}
-
-	private static void getColor(InputStream in, byte[] buffer) throws Exception {
-		int c = in.read(buffer, 0, buffer.length);
-		if ((c == -1) || (c != buffer.length)) throw new EOFException("Read error");
-	}
-
-	private static void writeValueTo(
-							DcmParser parser,
-							byte[] buffer,
-							OutputStream out,
-							boolean swap) throws Exception {
-		InputStream in = parser.getInputStream();
-		int len = parser.getReadLength();
-		if (swap && (len & 1) != 0) {
-			throw new Exception(
-				"Illegal length for swapping value bytes: " + len);
-		}
-		if (buffer == null) {
-			if (swap) {
-				int tmp;
-				for (int i=0; i<len; ++i, ++i) {
-					tmp = in.read();
-					out.write(in.read());
-					out.write(tmp);
+			else {
+				for (int i=0; i<nEntries; i++) {
+					color[i] = (byte)((data[i] >> 8) & 0xff);
 				}
-			} else {
-				for (int i=0; i<len; ++i) {
-					out.write(in.read());
-				}
+			}				
+			logger.info("desc = "+nEntries+"; "+firstValue+"; "+nBits);
+		}
+		
+		public byte get(int pd) {
+			pd = pd & 0xffff;
+			if (pd < firstValue) return color[0];
+			if (pd < firstValue + nEntries) {
+				return color[pd-firstValue];
 			}
-		} else {
-			byte tmp;
-			int c, remain = len;
-			while (remain > 0) {
-				c = in.read(buffer, 0, Math.min(buffer.length, remain));
-				if (c == -1) {
-					throw new EOFException("EOF while reading element value");
-				}
-				if (swap) {
-					if ((c & 1) != 0) {
-						buffer[c++] = (byte) in.read();
-					}
-					for (int i=0; i<c; ++i, ++i) {
-						tmp = buffer[i];
-						buffer[i] = buffer[i + 1];
-						buffer[i + 1] = tmp;
-					}
-				}
-				out.write(buffer, 0, c);
-				remain -= c;
-			}
-		}
-		parser.setStreamPosition(parser.getStreamPosition() + len);
+			return color[color.length-1];
+		}		
 	}
-
 }
